2217e84602c86247e2e2b623cd64b30d3d1f080a
KikiVinko <kiki.vinko74@gmail.com>
2025-11-16 20:55:44 +0100

    login page test

--- NepremicnineProjekt/Program.cs
+++ NepremicnineProjekt/Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "gg2ez");
-app.MapGet("/health", () => "Healthy");
+app.UseStaticFiles();
+
+app.MapGet("/", () => Results.Redirect("/login.html"));
+
+app.MapPost("/login", async (HttpRequest request) =>
+{
+    var form = await request.ReadFormAsync();
+    var username = form["username"];
+    var password = form["password"];
+    
+    if(username == "admin" && password == "1234")
+        return Results.Text("<h1>Login successful!</h1>", "text/html");
+    else
+        return Results.Text("<h1>Invalid credentials!</h1>", "text/html");
+});
 
 app.Run("http://*:8080");
+
+
