3cf5009e0d3dd6c3d86ae5f09c22a36777c40d8c
KikiVinko <kiki.vinko74@gmail.com>
2025-11-19 13:06:53 +0100

    gl

--- NepremicnineProjekt/Classes/User.cs
+++
@@ -1,8 +0,0 @@
-
-public class User
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-    public int[] SavedPostsID { get; set; }
-}
